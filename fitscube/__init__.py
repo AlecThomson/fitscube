010ee3525ba7fdf000dc816c684c6408bbde7fd6
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2024-07-24 15:30:35 +0800

    Rename

--- fitscube/__init__.py
+++ fitscube/__init__.py
@@ -5,7 +5,3 @@ from __future__ import annotations
 from ._version import version as __version__
 
 __all__ = ["__version__"]
-
-
-from .fitscube import combine_fits  # noqa: F401
-from .stokescube import combine_stokes  # noqa: F401
