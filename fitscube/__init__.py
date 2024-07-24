c7ff00914b7121243bd9c57ebe5622fc2d62a332
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2024-07-24 10:54:00 +0800

    ruff

--- fitscube/__init__.py
+++ fitscube/__init__.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
-from .fitscube import combine_fits as combine_fits
-from .stokescube import combine_stokes as combine_stokes
+from .fitscube import combine_fits
+from .stokescube import combine_stokes
