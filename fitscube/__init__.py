9ea56c60cbd4fe89df304d5b7528082f47e47491
Alec Thomson <AlecThomson@users.noreply.github.com>
2024-12-04 16:05:14 +0800

    Add combine (#15)

--- fitscube/__init__.py
+++ fitscube/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from fitscube.combine_fits import combine_fits
+
 from ._version import version as __version__
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "combine_fits"]
