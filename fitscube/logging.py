348688a72491f5e7e6931485bd688aacb08dc8d3
Alec Thomson <AlecThomson@users.noreply.github.com>
2024-12-05 15:11:22 +0800

    Fix logging and write (#16)
    * No stokes
    
    * Fix logger and file write
    
    * Missed coro
    
    * logger pyline
    
    * Global lint disable

--- fitscube/logging.py
+++ fitscube/logging.py
@@ -10,29 +10,7 @@ logging.captureWarnings(True)
 # https://gist.github.com/gwerbin/e9ab7a88fef03771ab0bf3a11cf921bc
 
 
-def setup_logger() -> logging.Logger:
-    """Setup a logger.
-
-    Args:
-        filename (Optional[str], optional): Output log file. Defaults to None.
-
-    Returns:
-        logging.Logger: The logger
-    """
-    logger = logging.getLogger("cutout_fits")
-    logger.setLevel(logging.WARNING)
-    formatter = logging.Formatter(
-        fmt="[%(threadName)s] %(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-
-    ch = logging.StreamHandler()
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
-
-    return logger
-
-
+# pylint: disable=W0621
 def set_verbosity(logger: logging.Logger, verbosity: int) -> None:
     """Set the logger verbosity.
 
@@ -50,3 +28,15 @@ def set_verbosity(logger: logging.Logger, verbosity: int) -> None:
         level = logging.CRITICAL
 
     logger.setLevel(level)
+
+
+logger = logging.getLogger("cutout_fits")
+logger.setLevel(logging.WARNING)
+formatter = logging.Formatter(
+    fmt="[%(threadName)s] %(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
